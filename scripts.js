6e5315600618b62dc036f58389adbd92cb4301cf
kelly <kelly.n.waite@gmail.com>
2017-01-04 13:07:26 -0700

    Name variables for html elements; Add pause/play functionality

---
+++ scripts.js
@@ -0,0 +1,33 @@
+// Get our elements
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const toggle = document.querySelector('.toggle');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const ranges = document.querySelectorAll('.player__slider');
+
+
+
+// Build our functions
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+  // Same as above
+  // if(video.paused) {
+  //   video.play();
+  // } else {
+  //   video.pause();
+  // }
+}
+
+function toggleButton() {
+  const method = video.paused ? toggle.innerHTML = '►': toggle.innerHTML = '⏸';
+}
+
+
+// Hook up event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('click', toggleButton);
+toggle.addEventListener('click', togglePlay);
+toggle.addEventListener('click', toggleButton);
