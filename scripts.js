256c8b8f2552a1432fafe60cfadc0d1fb80e12eb
kelly <kelly.n.waite@gmail.com>
2017-01-04 13:47:47 -0700

    Add my solution for rangeUpdates

--- scripts.js
+++ scripts.js
@@ -22,12 +22,37 @@ function togglePlay() {
 }
 
 function toggleButton() {
-  const method = video.paused ? toggle.innerHTML = '►': toggle.innerHTML = '⏸';
+  // Kelly's Solution
+  // const icon = video.paused ? toggle.innerHTML = '►': toggle.innerHTML = '⏸';
+  const icon = this.paused ? '▶️' : '⏸';
+  toggle.textContent = icon;
 }
 
+function skip() {
+  // Kelly's Solution
+  // if (this.dataset.skip=== "25") {
+  //   video.currentTime= video.currentTime + 25;
+  // } else if (this.dataset.skip === "-10") {
+  //   video.currentTime = video.currentTime - 10;
+  // } else {
+  //   return;
+  // }
+  video.currentTime +=  parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate () {
+  console.log(this.name);
+  const volume = this.name === "volume" ? video.volume = this.value : null;
+  const playbackRate = this.name === "playbackRate" ? video.playbackRate = this.value : null;
+}
 
 // Hook up event listeners
 video.addEventListener('play', toggleButton);
 video.addEventListener('pause', toggleButton);
 video.addEventListener('click', togglePlay);
+
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(c => c.addEventListener('click', skip));
+
+ranges.forEach( c => c.addEventListener('change', handleRangeUpdate));
